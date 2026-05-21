d45da9c61bf9f2eb99c663de753b31825b271ef7
quachvantu <quachvantu@gmail.com>
2026-05-21 23:54:59 +0700

    feat: implement bird jump mechanic

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float jump = 5f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+    }
+}
